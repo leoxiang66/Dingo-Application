9885918095bb4cf2fabb9bc6fdd3e42c34fc4e06
Leon <xiang.tao@outlook.de>
2025-04-02 17:11:03 +0800

    Update App.svelte

--- dingo/frontend/src/App.svelte
+++ dingo/frontend/src/App.svelte
@@ -6,7 +6,7 @@
   import {page_idx } from "./stores";
 </script>
 
-<div class="w-screen h-screen overflow-hidden bg-white">
+<div class="w-screen h-screen overflow-hidden bg-white font-mono">
   {#if $page_idx === 0}
     <Home />
     {:else if $page_idx === 1}
